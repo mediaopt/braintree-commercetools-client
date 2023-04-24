a37354731dd42cabee81babce00bb1352e30d573
Majid Abbasi <majidabbasi@Majids-MacBook-Air.local>
2023-04-24 16:43:24 +0400

    Changed app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <CreditCard
-        getClientTokenUrl="/mock-api/getClientToken.json"
+        getClientTokenUrl="/getClientToken"
         paymentId="01c5c18a-1495-46bd-bc48-68d8b2470a2b"
         paymentVersion={1}
       />
