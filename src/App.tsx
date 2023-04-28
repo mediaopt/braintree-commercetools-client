ebfb4533b7aaae0feda8b2a1d2af09bb39b9a23c
Majid Abbasi <majidabbasi@Majids-MacBook-Air.local>
2023-04-28 19:40:10 +0400

    removed data

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
         getClientTokenUrl="https://poc-majid-mediaopt.frontastic.dev/frontastic/action/payment/getClientToken"
         purchaseUrl="https://poc-majid-mediaopt.frontastic.dev/frontastic/action/payment/purchase"
         sessionKey="frontastic-session"
-        sessionValue="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ3aXNobGlzdElkIjoiNWY0NzhlYzctZTYyMC00ZWQ2LWJjY2QtODNkZjIxMTlmMmNhIiwiY2FydElkIjoiMmM0ZDRhNGYtOGUyMS00MDU3LWFmYjAtNTcwYWRlZTQ5Mjc3In0.nUSwua-lGef509sF8ccmLcx84n--rC02zboLX_SpRo8"
+        sessionValue="XXXXX"
       />
     </div>
   );
