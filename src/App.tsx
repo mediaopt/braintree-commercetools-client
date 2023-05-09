45fb35319b9c33dcc180e97003138096b8588f1e
Majid Abbasi <majidabbasi@Majids-MacBook-Air.local>
2023-05-09 12:52:44 +0400

    #33981 added first version of Result page

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
         getClientTokenUrl="https://poc-majid-mediaopt.frontastic.dev/frontastic/action/payment/getClientToken"
         purchaseUrl="https://poc-majid-mediaopt.frontastic.dev/frontastic/action/payment/purchase"
         sessionKey="frontastic-session"
-        sessionValue="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjYXJ0SWQiOiIxODA0MGZjYS0xYzZiLTQ5Y2EtOTE2Ny0xMTcwNTYzMGIxNDkiLCJ3aXNobGlzdElkIjoiM2EwM2VhNjEtOTgxNC00YTQ1LThiMDYtMmM5YjZlN2ViZjQwIn0.Dhx0yXIqS0WYpBMNmp2q9odsInRN0-M_p7LkhhRn8-4"
+        sessionValue="XXXXX"
         purchaseCallback={() => {
           console.log("Do something");
         }}
