c895a610ed952863dc38727d910e599c985dbb8f
Jonathan Yeboah <jonathan.yeboah@mediaopt.de>
2023-06-07 10:49:31 +0200

    #34137 only show venmo button after checks have passed

--- src/components/Venmo/VenmoMask.tsx
+++ src/components/Venmo/VenmoMask.tsx
@@ -176,6 +176,7 @@ export const VenmoMask: React.FC<React.PropsWithChildren<VenmoMaskType>> = ({
           "justify-center align-center rounded-md px-4 py-2 text-sm font-medium focus:outline-none focus:ring-2 focus:ring-blue-500 text-white bg-blue-500 hover:bg-blue-600  shadow-sm":
             true,
           "w-full": fullWidth,
+          hidden: !displayButton,
         })}
         id="submit"
       >
