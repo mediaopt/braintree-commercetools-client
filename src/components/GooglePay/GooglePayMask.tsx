e5deaf5ce2a28e82804a5935a3833de127c8662f
Jonathan Yeboah <jonathan.yeboah@mediaopt.de>
2023-06-08 10:01:59 +0200

    #34145 code review changes

--- src/components/GooglePay/GooglePayMask.tsx
+++ src/components/GooglePay/GooglePayMask.tsx
@@ -27,107 +27,112 @@ export const GooglePayMask: React.FC<
 
   const googlePayButtonContainer = React.useRef<HTMLDivElement>(null);
 
-  const createGooglePay = () => {
-    let paymentsClient = new google.payments.api.PaymentsClient({
-      environment: environment,
-    });
-
-    braintreeClient.create(
-      {
-        authorization: clientToken,
-      },
-      function (clientErr, clientInstance) {
-        let googlePayCreateOptions = googleMerchantId
-          ? {
-              googleMerchantId: googleMerchantId,
-            }
-          : {};
-        googlePayment.create(
-          {
-            client: clientInstance,
-            googlePayVersion: GooglePayVersion,
-            ...googlePayCreateOptions,
-          },
-          function (googlePaymentErr, googlePaymentInstance) {
-            paymentsClient
-              .isReadyToPay({
-                apiVersion: GoogleApiVersion,
-                apiVersionMinor: GoogleApiMinorVersion,
-                allowedPaymentMethods:
-                  googlePaymentInstance.createPaymentDataRequest()
-                    .allowedPaymentMethods,
-              })
-              .then(function (response) {
-                if (response.result) {
-                  const googlePayButton = paymentsClient.createButton({
-                    onClick: (e) => {
-                      e.preventDefault();
-
-                      let paymentDataRequest =
-                        googlePaymentInstance.createPaymentDataRequest({
-                          transactionInfo: {
-                            currencyCode: paymentInfo.currency,
-                            totalPriceStatus: totalPriceStatus,
-                            totalPrice: paymentInfo.amount.toString(),
-                            countryCode: acquirerCountryCode,
-                          },
-                        });
-
-                      let cardPaymentMethod =
-                        paymentDataRequest.allowedPaymentMethods[0];
-                      cardPaymentMethod.parameters.billingAddressRequired =
-                        billingAddressRequired;
-                      cardPaymentMethod.parameters.billingAddressParameters = {
-                        format: billingAddressFormat,
-                        phoneNumberRequired: phoneNumberRequired,
-                      };
-
-                      paymentsClient
-                        .loadPaymentData(paymentDataRequest)
-                        .then((paymentData) => {
-                          googlePaymentInstance.parseResponse(
-                            paymentData,
-                            function (err: any, result: any) {
-                              if (err) {
-                                notify("Error", err.message);
-                                return;
-                              }
-                              handlePurchase(result.nonce);
-                            }
-                          );
-                        })
-                        .catch(function (err) {
-                          notify("Error", err.message);
-                        });
-                    },
-                    buttonColor: buttonTheme,
-                    buttonType: buttonType,
-                  });
-                  if (googlePayButtonContainer.current) {
-                    googlePayButtonContainer.current.after(googlePayButton);
-                  }
-                } else {
-                  notify("Error", "Failed payment call. Retry");
-                }
-              })
-              .catch(function (err) {
-                notify("Error", err.message);
-              });
-          }
-        );
-      }
-    );
-  };
-
   useEffect(() => {
-    loadScript("https://pay.google.com/gp/p/js/pay.js").then(() =>
-      createGooglePay()
-    );
-  }, []);
+    loadScript("https://pay.google.com/gp/p/js/pay.js").then(() => {
+      let paymentsClient = new google.payments.api.PaymentsClient({
+        environment: environment,
+      });
 
-  return (
-    <>
-      <div ref={googlePayButtonContainer} />
-    </>
-  );
+      braintreeClient.create(
+        {
+          authorization: clientToken,
+        },
+        function (clientErr, clientInstance) {
+          let googlePayCreateOptions = googleMerchantId
+            ? {
+                googleMerchantId: googleMerchantId,
+              }
+            : {};
+          googlePayment.create(
+            {
+              client: clientInstance,
+              googlePayVersion: GooglePayVersion,
+              ...googlePayCreateOptions,
+            },
+            function (googlePaymentErr, googlePaymentInstance) {
+              paymentsClient
+                .isReadyToPay({
+                  apiVersion: GoogleApiVersion,
+                  apiVersionMinor: GoogleApiMinorVersion,
+                  allowedPaymentMethods:
+                    googlePaymentInstance.createPaymentDataRequest()
+                      .allowedPaymentMethods,
+                })
+                .then(function (response) {
+                  if (response.result) {
+                    const googlePayButton = paymentsClient.createButton({
+                      onClick: (e) => {
+                        e.preventDefault();
+
+                        let paymentDataRequest =
+                          googlePaymentInstance.createPaymentDataRequest({
+                            transactionInfo: {
+                              currencyCode: paymentInfo.currency,
+                              totalPriceStatus: totalPriceStatus,
+                              totalPrice: paymentInfo.amount.toString(),
+                              countryCode: acquirerCountryCode,
+                            },
+                          });
+
+                        let cardPaymentMethod =
+                          paymentDataRequest.allowedPaymentMethods[0];
+                        cardPaymentMethod.parameters.billingAddressRequired =
+                          billingAddressRequired;
+                        cardPaymentMethod.parameters.billingAddressParameters =
+                          {
+                            format: billingAddressFormat,
+                            phoneNumberRequired: phoneNumberRequired,
+                          };
+
+                        paymentsClient
+                          .loadPaymentData(paymentDataRequest)
+                          .then((paymentData) => {
+                            googlePaymentInstance.parseResponse(
+                              paymentData,
+                              function (err: any, result: any) {
+                                if (err) {
+                                  notify("Error", err.message);
+                                  return;
+                                }
+                                handlePurchase(result.nonce);
+                              }
+                            );
+                          })
+                          .catch(function (err) {
+                            notify("Error", err.message);
+                          });
+                      },
+                      buttonColor: buttonTheme,
+                      buttonType: buttonType,
+                    });
+                    if (googlePayButtonContainer.current) {
+                      googlePayButtonContainer.current.after(googlePayButton);
+                    }
+                  } else {
+                    notify("Error", "Failed payment call. Retry");
+                  }
+                })
+                .catch(function (err) {
+                  notify("Error", err.message);
+                });
+            }
+          );
+        }
+      );
+    });
+  }, [
+    environment,
+    clientToken,
+    googleMerchantId,
+    buttonTheme,
+    buttonType,
+    paymentInfo,
+    totalPriceStatus,
+    phoneNumberRequired,
+    billingAddressRequired,
+    billingAddressFormat,
+    acquirerCountryCode,
+  ]);
+
+  return <div ref={googlePayButtonContainer} />;
 };
