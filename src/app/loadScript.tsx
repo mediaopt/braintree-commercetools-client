3f9335874f283aa4bb95d27fcc899c2ad00e7198
Jonathan Yeboah <jonathan.yeboah@mediaopt.de>
2023-07-14 11:27:31 +0200

    #34256 error handling for script loading

--- src/app/loadScript.tsx
+++ src/app/loadScript.tsx
@@ -4,6 +4,7 @@ const loadScript = (url: string): Promise<boolean> => {
     script.src = url;
     script.async = true;
     script.onload = () => resolve(true);
+    script.onerror = () => resolve(false);
     document.body.appendChild(script);
   });
 };
